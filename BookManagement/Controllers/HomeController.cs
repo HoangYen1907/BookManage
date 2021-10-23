5dd4efe133fb2b7ce280c0ad45f35196c667b1da
HoangYen1907 <62740875+HoangYen1907@users.noreply.github.com>
2021-10-23 08:52:39 +0700

    update FE

--- BookManagement/Controllers/HomeController.cs
+++ BookManagement/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using BookManagement.Models;
+﻿using BookManagement.Common;
+using BookManagement.Data;
+using BookManagement.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,11 +15,11 @@ namespace BookManagement.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly BookManagementDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(BookManagementDbContext db)
         {
-            _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
@@ -30,14 +33,22 @@ namespace BookManagement.Controllers
             return View();
         }
 
+        
         public IActionResult NDSach(int id)
         {
             ViewBag.BookId = id;
             return View();
         }
-
+        
         public IActionResult NDChuong(int id)
         {
+            Chapter Exsits = _db.Chapters.Where(x => x.ChapterId == id).FirstOrDefault();
+            Chapter Item = Exsits;
+            Item.TotalView = Item.TotalView + 1;
+            _db.Entry(Exsits).State = EntityState.Detached;
+            _db.Entry(Item).State = EntityState.Modified;
+            _db.SaveChanges();
+
             ViewBag.ChapterId = id;
             return View();
         }
@@ -48,10 +59,28 @@ namespace BookManagement.Controllers
             return View();
         }
 
-        public IActionResult TimKiem(int key)
+        public JsonResult TimKiem(string keyword)
         {
-            ViewBag.Key = key;
-            return View();
+            Message msg = new Message();
+            try
+            {
+                keyword = string.IsNullOrEmpty(keyword) ? "" : keyword.Trim();
+                List<Book> booknew = _db.Books.Where(x => x.BookName.Contains(keyword)).OrderBy(x => x.ModifyTime).Take(5).ToList();
+                List<Book> bookcompleted = _db.Books.Where(x => x.BookName.Contains(keyword) && x.IsCompleted == true).Take(5).ToList();
+
+                //data = booknew
+                msg.Data = booknew.Select(x => new { x.BookName, x.BookId, x.TotalChapter, x.BookImage, x.Author });
+                //subdata = completed
+                msg.SubData = bookcompleted.Select(x => new { x.BookName, x.BookId, x.TotalChapter, x.BookImage, x.Author });
+                msg.Header.MsgType = MessageType.Success;
+                return Json(msg);
+            }
+            catch (Exception ex)
+            {
+                msg.Header.MsgType = MessageType.ServerError;
+                msg.Description = ex.Message;
+                return Json(msg);
+            }
         }
 
         public IActionResult SachMoiCapNhat()
